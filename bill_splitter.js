16510ccaca1cf798a7f8b1cd8d9d5656b1800733
Abylkhair03 <nurbina@usf.edu>
2024-09-08 16:18:48 -0400

    firstcommit

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,1 +1,23 @@
+//Calculate the tip using ternary operator
+let bill = 275;
+let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+console.log('Initial logic for calculating tip using ternary ');
+
+
+console.log("The bill was " + bill + ", the tip was " + tip + ", and the total value is " + (bill + tip));
+
+function calculateTip(bill) {
+    return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+}
+
+console.log("Tip for $100 bill: " + calculateTip(100));
+
+
+const bills = [275, 40, 430, 125, 555, 44];
+const tips = bills.map(bill => calculateTip(bill));
+const total = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Bills: " + bills); 
+console.log("Tips: " + tips);
+console.log("Total: " + total);
 
